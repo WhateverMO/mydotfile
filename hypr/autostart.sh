5260d1e18df9d9b8b8ea36c968576247ffdd6eba
WhateverMO <2447583173@qq.com>
2023-02-13 00:33:28 +0800

    hyprland screen sharing but screencut not work

--- hypr/autostart.sh
+++ hypr/autostart.sh
@@ -5,4 +5,11 @@ udiskie &
 fcitx5 &
 killall foot &
 foot --server &
+sleep 1
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
 
