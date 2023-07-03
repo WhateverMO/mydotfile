c65d31338f58a8c015968659b9fbe2001fcb810c
WhateverMO <2447583173@qq.com>
2023-07-03 20:27:07 +0800

    sync.fish update

--- fish/sync.fish
+++ fish/sync.fish
@@ -1,3 +1,5 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher remove ilancosman/tide@v5
 fisher install ilancosman/tide@v5
+git rm fish_variables --cached
+rm fishd.tmp.*
