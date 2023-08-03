41c7db44de74ffcea15cb70b3cb9eebae8a995c2
WhateverMO <2447583173@qq.com>
2023-08-03 12:19:06 +0800

    modify sddm aerial theme use.sh

--- sddm-theme/aerial-sddm-theme/use.sh
+++ sddm-theme/aerial-sddm-theme/use.sh
@@ -4,9 +4,9 @@ yay -S --noconfirm sddm gst-libav phonon-qt5-gstreamer gst-plugins-good qt5-quic
 git clone git@github.com:3ximus/aerial-sddm-theme.git
 sudo mv aerial-sddm-theme /usr/share/sddm/themes
 sudo rm /usr/share/sddm/themes/aerial-sddm-theme/theme.conf.user
-cp ./theme.conf.user /usr/share/sddm/themes/aerial-sddm-theme/
-mkdir /etc/sddm.conf.d/
-cp ./sddm.conf /etc/sddm.conf.d/sddm.conf
+sudo cp ./theme.conf.user /usr/share/sddm/themes/aerial-sddm-theme/
+sudo mkdir /etc/sddm.conf.d/
+sudo cp ./sddm.conf /etc/sddm.conf.d/sddm.conf
 systemctl disable display-manager.service
 systemctl enable sddm
 rm -rf ./aerial-sddm-theme/
