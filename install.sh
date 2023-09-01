ce983f9c0543b4fb8c4f169834578daf646f59bd
WhateverMO <2447583173@qq.com>
2023-09-01 19:28:36 +0800

    just write install.sh basicly

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ln -s "$(pwd)/home/.bash_profile" ~/.bash_profile
+ln -s "$(pwd)/dunst" ~/.config/dunst
+ln -s "$(pwd)/fish" ~/.config/fish
+ln -s "$(pwd)/foot" ~/.config/foot
+ln -s "$(pwd)/hypr" ~/.config/hypr
+ln -s "$(pwd)/kitty" ~/.config/kitty
+ln -s "$(pwd)/myUtils" ~/.config/myUtils
+ln -s "$(pwd)/neofetch" ~/.config/neofetch
+ln -s "$(pwd)/waybar" ~/.config/waybar
+
