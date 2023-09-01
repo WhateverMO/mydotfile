7daa81093ecfce66ad1dec8fd068321bd0d1b4f1
WhateverMO <2447583173@qq.com>
2023-09-01 13:31:07 +0800

    update hyprland screen shoot

--- hypr/screenShot.sh
+++ hypr/screenShot.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 region(){
-	grim -l 0 ~/tempScreenShot.png && hyprctl dispatch exec "imv -f ~/tempScreenShot.png" && grim -g "$(slurp)" ~/nowScreenShot.png
-	if [[ $? == 1 ]]; then
-		kill -9 $(pgrep imv | tail -n 1)
-	else
-		kill -9 $(pgrep imv | tail -n 1) && swappy -f ~/nowScreenShot.png 
+	grim -g "$(slurp)" ~/nowScreenShot.png
+	if [[ $? != 1 ]]; then
+		swappy -f ~/nowScreenShot.png 
 	fi
 }
 
 all(){
-	grim -l 0 ~/tempScreenShot.png && cp ~/tempScreenShot.png ~/nowScreenShot.png && swappy -f ~/nowScreenShot.png 
+	grim -l 0 ~/nowScreenShot.png && swappy -f ~/nowScreenShot.png 
 }
 
 if [[ "$1" == "--region" ]]; then
