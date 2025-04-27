59c76d12a5e09584004cb667faf72ba30402deda
WhateverMO_rog <75033001+WhateverMO@users.noreply.github.com>
2025-04-27 19:40:41 +0800

    starship

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-ln -s "$(pwd)/home/.bash_profile" ~/.bash_profile
-ln -s "$(pwd)/dunst" ~/.config/dunst
-ln -s "$(pwd)/fish" ~/.config/fish
-ln -s "$(pwd)/foot" ~/.config/foot
-ln -s "$(pwd)/hypr" ~/.config/hypr
-ln -s "$(pwd)/kitty" ~/.config/kitty
-ln -s "$(pwd)/myUtils" ~/.config/myUtils
-ln -s "$(pwd)/neofetch" ~/.config/neofetch
-ln -s "$(pwd)/waybar" ~/.config/waybar
-ln -s "$(pwd)/wayvnc" ~/.config/wayvnc
+# if not exists, ln it in one line
+[ -f ~/.bash_profile ] && ln -sf "$(pwd)/home/.bash_profile" ~/.bash_profile
+[ -f ~/.config/dunst ] && ln -sf "$(pwd)/dunst" ~/.config/dunst
+[ -f ~/.config/fish ] && ln -sf "$(pwd)/fish" ~/.config/fish
+[ -f ~/.config/foot ] && ln -sf "$(pwd)/foot" ~/.config/foot
+[ -f ~/.config/hypr ] && ln -sf "$(pwd)/hypr" ~/.config/hypr
+[ -f ~/.config/kitty ] && ln -sf "$(pwd)/kitty" ~/.config/kitty
+[ -f ~/.config/myUtils ] && ln -sf "$(pwd)/myUtils" ~/.config/myUtils
+[ -f ~/.config/neofetch ] && ln -sf "$(pwd)/neofetch" ~/.config/neofetch
+[ -f ~/.config/waybar ] && ln -sf "$(pwd)/waybar" ~/.config/waybar
+[ -f ~/.config/wayvnc ] && ln -sf "$(pwd)/wayvnc" ~/.config/wayvnc
+[ -f ~/.config/starship.toml ] && ln -sf "$(pwd)/starship/starship.toml" ~/.config/starship.toml
