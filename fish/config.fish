61ab1315e410ef476f906fbe64ccfcdc06bc6d1b
WhateverMO <75033001+WhateverMO@users.noreply.github.com>
2026-05-06 10:25:27 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -17,6 +17,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+zoxide init fish | source
 starship init fish | source
 # enable_transience
 
@@ -41,3 +42,4 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+set -gx XDG_CONFIG_HOME "$HOME/.config"
